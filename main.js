a236eebb28c759dc2f0f1dce0ab00f004b159626
Paul Conway <paulc00@gmail.com>
2024-03-15 18:05:43 -0400

    Revert alert message

--- main.js
+++ main.js
@@ -4,15 +4,10 @@ searchGoogleMaps = function (word) {
 
   // console.log(query);
 
-  if (query) {
     chrome.tabs.create({
       url: encodeURI("https://www.google.com/maps/search/?api=1&query="
         + query)
     });
-  } else {
-    // If selection is empty, error
-    alert("No address is selected. Please select an address before selecting Search Google Maps.");
-  }
 };
 
 chrome.contextMenus.removeAll(function () {
