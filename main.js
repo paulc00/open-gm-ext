0de893a51cab0cf8142081b5aa2c1054ca864719
Paul Conway <paulc00@gmail.com>
2024-03-15 16:47:39 -0400

    Basic App

---
+++ main.js
@@ -0,0 +1,18 @@
+searchGoogleMaps = function(word){
+  const query = word.selectionText;
+
+  console.log(query);
+  // check if there is a comma after the street
+
+  chrome.tabs.create({url: encodeURI("https://www.google.com/maps/search/?api=1&query="
++ query)});
+};
+
+chrome.contextMenus.removeAll(function() {
+  chrome.contextMenus.create({
+   id: "1",
+   title: "Open in Google Maps",
+   contexts:["selection"],  // ContextType
+  }); })
+
+chrome.contextMenus.onClicked.addListener(searchGoogleMaps);
